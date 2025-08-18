1b703eee28e8827f2ba0f8735d3c807475ba1b5b
frasero <neironogamiiko@gmail.com>
2025-08-18 17:12:15 +0300

    1-st and 2-nd ex

---
+++ Ex1.py
@@ -0,0 +1,43 @@
+import cv2; import torch
+import numpy as np
+
+# scalar
+scalar = torch.tensor(7)
+print(f"Scalar is: {scalar}")
+print(f"Scalar number of dimensions: {scalar.ndim}"); print(f"Scalar value: {scalar.item()}")
+print(f"Shape of scalar: {scalar.shape}")
+
+print("**************************************\n")
+
+# vector
+vector = torch.tensor([7,7])
+print(f"Vector is: {vector}"); print(f"Vector number of dimensions: {vector.ndim}")
+print(f"Shape of vector: {vector.shape}")
+
+print("**************************************\n")
+
+# MATRIX
+MATRIX = torch.tensor([[7, 8],
+                       [9,10]])
+
+print(f"Matrix is:\n{MATRIX}"); print(f"Matrix number of dimensions: {MATRIX.ndim}")
+print(f"Shape of matrix: {MATRIX.shape}")
+
+print("**************************************\n")
+
+# TENSOR
+TENSOR = torch.tensor([[[1, 2, 3],
+                        [4,5,6],
+                        [7,8,9]]])
+
+print(f"Tensor is:\n{TENSOR}"); print(f"Tensor number of dimensions: {TENSOR.ndim}")
+print(f"Shape of tensor: {TENSOR.shape}")
+
+print("**************************************\n")
+
+# Random tensors
+random_TENSOR = torch.rand(1, 3, 4)
+print(random_TENSOR)
+# Random tensors with similar shape to an image tensor
+random_image_size_TENSOR = torch.rand(size=(3, 224, 224))
+print(random_image_size_TENSOR.shape, random_image_size_TENSOR.ndim)
---
+++ Ex2.py
@@ -0,0 +1,8 @@
+import cv2; import torch
+import numpy as np
+
+zeros = torch.zeros(3,4)
+random_tensor = torch.rand(3,4)
+ones = torch.ones(3,4)
+
+print(ones)
