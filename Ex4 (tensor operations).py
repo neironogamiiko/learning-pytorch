9644def279969b2dfda4e581e07f8bdb83647223
Frasero <neironogamiiko@gmail.com>
2025-08-19 16:22:19 +0300

    base structure for Ex4

---
+++ Ex4 (tensor operations).py
@@ -0,0 +1,8 @@
+import cv2; import torch
+import numpy as np
+from sklearn.externals.array_api_compat import device
+
+print(f"\nCUDA available: {torch.cuda.is_available()}")
+print(f"Device ID: {torch.cuda.current_device()}")
+print(f"Device name: {torch.cuda.get_device_name(0)}\n")
+cuda = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
