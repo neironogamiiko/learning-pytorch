f0c57108bea4df93062a54a173a412df2564016a
Frasero <neironogamiiko@gmail.com>
2025-08-19 15:33:51 +0300

    rename Ex and made 3rd Ex base structure

--- Ex1 (scalar, vector, matrix, tensor).py
+++ Ex1 (scalar, vector, matrix, tensor).py
@@ -1,6 +1,11 @@
 import cv2; import torch
 import numpy as np
 
+print(f"\nCUDA available: {torch.cuda.is_available()}")
+print(f"Device ID: {torch.cuda.current_device()}")
+print(f"Device name: {torch.cuda.get_device_name(0)}\n")
+cuda = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 # scalar
 scalar = torch.tensor(7)
 print(f"Scalar is: {scalar}")
