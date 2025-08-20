5e69fc1986d7ea1bbad4a0adb2bd918a70d8f345
Frasero <neironogamiiko@gmail.com>
2025-08-20 13:52:01 +0300

    Make Ex5

--- Ex5 (matrix multiplication).py
+++ Ex5 (matrix multiplication).py
@@ -8,11 +8,11 @@ cuda = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Element-wise multiplication
 tensor = torch.tensor([1,2,3], device=cuda, dtype=torch.float32)
-print(tensor*tensor)
+print(f"Element-wise multiplication: {tensor * tensor}\n")
 
 # Matrix multiplication [n x m] * [m x n]
-print(torch.matmul(tensor, tensor))
-print(tensor @ tensor) # @ - it's the same as matmul
+print(f"Matrix multiplication: {torch.matmul(tensor, tensor)}")
+print(f"Also matrix multiplication: {tensor @ tensor}\n") # @ - it's the same as matmul
 
 # Two main rules that performing matrix multiplication needs to satisfy:
 # 1. The inner dimensions must match: [n x m] * [m x n]
@@ -22,3 +22,11 @@ print(tensor @ tensor) # @ - it's the same as matmul
 #   (2,3) @ (3,2) -> (2,2)
 
 # One of the most common errors in Deep Learning are shape errors.
+tensor_A = torch.tensor([[1, 2],
+                         [3, 4],
+                         [5, 6]], device=cuda, dtype=torch.float32)
+
+tensor_B = torch.tensor([[1, 2, 3],
+                         [4, 5, 6]], device=cuda, dtype=torch.float32)
+
+print(f"Matrix multiplicatin (3x2 and 2x3): \n{torch.matmul(tensor_A, tensor_B)}")
