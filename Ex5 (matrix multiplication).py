380fd809bfac59751d65b64974dd434cb48a3f1d
Frasero <neironogamiiko@gmail.com>
2025-08-26 15:58:53 +0300

    Make Ex5

--- Ex5 (matrix multiplication).py
+++ Ex5 (matrix multiplication).py
@@ -30,3 +30,28 @@ tensor_B = torch.tensor([[1, 2, 3],
                          [4, 5, 6]], device=cuda, dtype=torch.float32)
 
 print(f"Matrix multiplicatin (3x2 and 2x3): \n{torch.matmul(tensor_A, tensor_B)}")
+
+tensor_C = torch.tensor([[1,2,3],
+                         [1,2,3],
+                         [1,2,3]])
+
+tensor_D = torch.tensor([[4,5],
+                         [5,6],
+                         [1,3]])
+
+print(f"\nMultiplication result:\n{torch.mm(tensor_C, tensor_D)}") # the same as torch.matmul or @
+
+# To manipulate shape of one of the tensor - we can use transpose.
+print(f"Tensor D transpose:\n{tensor_D.T}")
+
+tensor_F = torch.tensor([[1,2],
+                         [3,4],
+                         [1,6]])
+
+tensor_G = torch.tensor([[4,5],
+                         [5,6],
+                         [1,3]])
+
+print(f"Original shape of tensor F: {tensor_F.shape}\nOriginal shape of tensor G: {tensor_G.shape}")
+print(f"\nTensor G transpose:\n{tensor_G.T}\nand it's new shape: {tensor_G.T.shape}\n")
+print(f"Multiplication result:\n{tensor_F @ tensor_G.T}")
