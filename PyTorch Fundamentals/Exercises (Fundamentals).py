e958f273dcfe4f0cb48947fa1bba4c53cfed79e9
Frasero <neironogamiiko@gmail.com>
2025-08-27 12:22:34 +0300

    Some commentrs to exercise

--- PyTorch Fundamentals/Exercises (Fundamentals).py
+++ PyTorch Fundamentals/Exercises (Fundamentals).py
@@ -1,52 +1,51 @@
-import torch; import numpy
+import torch; import os
 
 if torch.cuda.is_available():
     print(f"Device ID: {torch.cuda.current_device()}")
     print(f"Device name: {torch.cuda.get_device_name(0)}\n")
     device = 'cuda'
+    
+    os.system('nvidia-smi')
 else:
     device = 'cpu'
 
 # 2. Create a random tensor with shape (7, 7).
 tensor_A = torch.rand(7, 7)
-print(f"Tensor A:\n{tensor_A}")
+print(f"\nTensor A:\n{tensor_A}")
 
 # 3. Perform a matrix multiplication on the tensor from 2 with another
 # random tensor with shape (1, 7)
 tensor_B = torch.rand(1,7)
-print(f"Tensor B:\n{tensor_B}")
+print(f"\nTensor B:\n{tensor_B}")
 
 mmAB = torch.matmul(tensor_A, tensor_B.T)
-print(f"Multiplication result:\n{mmAB}")
+print(f"\nMultiplication result [7x7] * [1x7]:\n{mmAB}")
 
 # 4.1. Set the random seed to 0 and do exercises 2 & 3 over again.
 torch.manual_seed(0)
 tensor_A = torch.rand(7,7)
-print(f"Tensor A:\n{tensor_A}")
+print(f"\nTensor A:\n{tensor_A}")
 
 tensor_B = torch.rand(1,7)
-print(f"Tensor B:\n{tensor_B}")
+print(f"\nTensor B:\n{tensor_B}")
 
 mmAB = torch.matmul(tensor_A, tensor_B.T)
-print(f"Multiplication result:\n{mmAB}")
+print(f"\nMultiplication result with random seed 0:\n{mmAB}")
 
 # 4.2. Set the random seed to 0 for both tensors and do exercises 2 & 3 over again.
 torch.manual_seed(0)
 tensor_A = torch.rand(7,7)
-print(f"Tensor A:\n{tensor_A}")
+print(f"\nTensor A:\n{tensor_A}")
 
 torch.manual_seed(0)
 tensor_B = torch.rand(1,7)
-print(f"Tensor B:\n{tensor_B}")
+print(f"\nTensor B:\n{tensor_B}")
 
 mmAB = torch.mm(tensor_A, tensor_B.T)
-print(f"Multiplication result:\n{mmAB}")
+print(f"\nMultiplication result with random seed 0 for both tensors:\n{mmAB}")
 
 # 5. Speaking of random seeds, we saw how to set it with torch.manual_seed()
 # but is there a GPU equivalent? If there is, set the GPU random seed to 1234.
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(f"Cuda available: {torch.cuda.is_available()}")
 
 torch.cuda.manual_seed(1234)
 
@@ -55,15 +54,15 @@ torch.cuda.manual_seed(1234)
 torch.manual_seed(1234)
 
 tensor_A = torch.rand(2,3, device=device)
-print(f"Tensor A:\n{tensor_A}")
+print(f"\nTensor A:\n{tensor_A}")
 
 tensor_B = torch.rand(2,3, device=device)
-print(f"Tensor B:\n{tensor_B}")
+print(f"\nTensor B:\n{tensor_B}")
 
 # 7. Perform a matrix multiplication on the tensors you created in 6
 
 tensor_C = tensor_A @ tensor_B.T
-print(f"Multiplication result:\n{tensor_C}")
+print(f"\nMultiplication result on cuda:\n{tensor_C}\n")
 
 # 8. Find the maximum and minimum values of the output of 7.
 
@@ -83,11 +82,11 @@ print(f"Index of min tensor's value: {min_index_C}")
 
 torch.cuda.manual_seed(7)
 tensor_D = torch.rand(1,1,1,10, device=device)
-print(tensor_D)
+print(f"\nTensor D:\n{tensor_D}\n")
 # and then create a new tensor with all the 1 dimensions removed to be left
 # with a tensor of shape (10).
 tensor_D_squeezed = tensor_D.squeeze()
-print(tensor_D_squeezed.device)
+print(f"Squeezed tensor D device: {tensor_D_squeezed.device}\n")
 
 # Set the seed to 7 when you create it and print out the first tensor
 # and it's shape as well as the second tensor and it's shape.
