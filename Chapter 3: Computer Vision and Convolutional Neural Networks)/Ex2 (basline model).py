dd8387cef621d450b664cda56712bb73a4d0e049
Frasero <neironogamiiko@gmail.com>
2025-09-14 15:03:37 +0300

    flatten model as base structure for future model

---
+++ Chapter 3: Computer Vision and Convolutional Neural Networks)/Ex2 (basline model).py
@@ -0,0 +1,63 @@
+import torch
+from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+from torchvision import datasets, transforms
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+import matplotlib; matplotlib.use("TkAgg")
+
+# When starting to build a series of machine learning modelling experiments it's best practice to start with a baseline model.
+# A `baseline model` is a simple model you will try and improve upon with subsequent models/experiments.
+# In other words: start simply and add complexity when necessary.
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f"Current device: {device}")
+
+BATCH_SIZE = 32
+
+# Setup training data
+train_data = datasets.FashionMNIST(
+    root="data",            # where to download data to?
+    train=True,             # do we want the training dataset?
+    download=True,          # do we want to download yes/no?
+    transform=ToTensor(),   # how do we want to transform the data
+    target_transform=None   # how do we want to transform the labels/targets?
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+    target_transform=None
+)
+
+train_loader = DataLoader(
+    dataset=train_data,
+    batch_size=BATCH_SIZE,
+    shuffle=True
+)
+
+test_loader = DataLoader(
+    dataset=test_data,
+    batch_size=BATCH_SIZE,
+    shuffle=False
+)
+
+print(f"Length of train loader: {len(train_loader)} batches of {BATCH_SIZE}")
+print(f"Length of test loader: {len(test_loader)} batches of {BATCH_SIZE}")
+
+train_features_batch, train_labels_batch = next(iter(train_loader))
+
+# Create a flatten layer
+flatten_model = nn.Flatten()
+
+# Get a single sample
+x = train_features_batch[0]
+
+# Flatten the sample
+output = flatten_model(x) # perform a forward pass
+
+print(f"Shape before flattening: {x.shape} -> [color_channels, height, width]")
+print(f"Sha[e after flattening: {output.shape} -> [color_channels, height * width")
